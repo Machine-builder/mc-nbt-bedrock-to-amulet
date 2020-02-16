6bf52fa22fa5bb8fd0cf4aea34e8e48fcfa8f80d
Machine-builder <37518116+Machine-builder@users.noreply.github.com>
2020-02-16 16:40:30 +1100

    Add files via upload

---
+++ threeblocks_decoded.py
@@ -0,0 +1,98 @@
+'''
+{
+    "format_version": 1,
+    size: [3, 3, 3],
+    structure: {
+        "block_indices": [
+            [
+                0,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                2,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                1,
+                3
+            ],
+            [
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1,
+                -1
+            ]
+        ],
+        entities: [],
+        palette: {
+            default: {
+                "block_palette": [
+                    {
+                        name: "minecraft:gold_block",
+                        states: {},
+                        version: 17760256
+                    },
+                    {
+                        name: "minecraft:air",
+                        states: {},
+                        version: 17760256
+                    }, 
+                    {
+                        name: "minecraft:diamond_block",
+                        states: {},
+                        version: 17760256
+                    },
+                    {
+                        name: "minecraft:iron_block",
+                        states: {},
+                        version: 17760256
+                    }
+                ],
+                "block_position_data": {}
+            }
+        }
+    },
+    "structure_world_origin": [-15, 4, -15]
+}
+
+'''
